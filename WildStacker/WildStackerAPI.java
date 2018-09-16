e45a84243c6aac499c20e1f9c94da6f16c1d01c5
OmerBenGera <omerthepro126@gmail.com>
2018-09-16 19:04:12 +0300

    Added 1.13 support

--- WildStacker/WildStackerAPI.java
+++ WildStacker/WildStackerAPI.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.LivingEntity;
+
 import xyz.wildseries.wildstacker.api.objects.StackedEntity;
 import xyz.wildseries.wildstacker.api.objects.StackedItem;
 import xyz.wildseries.wildstacker.api.objects.StackedSpawner;
